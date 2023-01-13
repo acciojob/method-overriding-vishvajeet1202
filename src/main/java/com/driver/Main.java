ff024112a8b8e2c2a4fa8978b92325a8f014b68d
Vishvajeet <cvishwajeet1202@gmail.com>
2023-01-14 02:02:26 +0530

    solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,21 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+      B obj=new B();
+      String ans=obj.f1();
+      String ans2=obj.f1();
+  }
+  public static class A{
+
+
+      public static String f1(){
+          return "Invoking method from class A";
+      }
+  }
+  static class B extends A{
+      public static String f1(){
+          return "Method is overridden in Extendend class B";
+      }
+  }
 }
